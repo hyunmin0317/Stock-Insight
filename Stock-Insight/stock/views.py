98e60166a49a8cd62aa09570817957e67a904ba0
hyunmin0317 <choihm9903@naver.com>
2021-07-26 18:04:29 +0900

    Update icon

--- Stock-Insight/stock/views.py
+++ Stock-Insight/stock/views.py
@@ -4,17 +4,13 @@ from . import stock
 from django.http import JsonResponse
 from django.core.paginator import Paginator
 
+code_list = ['215000', '121440', '070960', '035250', '007720', '122450', '308100', '019010', '025980', '110790',
+             '294870', '288490', '183410', '206950', '300120', '005720', '017960', '034300', '081660']
+
+
 def home(request):
     page = request.GET.get('page', '1')  # 페이지
     stock_list = []
-    code_list = ['215000','121440','070960','035250','007720','122450','308100','019010','025980','110790','294870','288490','183410','206950','300120','005720','017960','034300','081660']
-
-    # stock.stock_delete()
-    # stock.stock_save()
-    # stock.information_delete()
-    # stock.information_save(code_list)
-    stock.date_delete()
-    stock.date_save()
 
     for code in code_list:
         try:
@@ -52,3 +48,15 @@ def sales(request):
 
     context = {'name_list': name_list, 'data_list':data_list}
     return render(request, 'stock/sales.html', context)
+
+def save_code(request):
+    stock.stock_delete()
+    stock.stock_save()
+    return render(request, 'save.html')
+
+def save_data(request):
+    stock.information_delete()
+    stock.information_save(code_list)
+    stock.date_delete()
+    stock.date_save()
+    return render(request, 'save.html')
